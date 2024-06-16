972b6a13df04fda2063c63eadf6825ef4008e390
jomebe <167706974+jomebe@users.noreply.github.com>
2024-06-17 08:25:54 +0900

    대충 ai작동만 되게 만듦

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,15 +1,2 @@
-for (i = 1; i <= 15; i++){
-	// 	for (j = 1; j <= 15; j++){
-	// 		if(field[i][j]==-2){//흑돌 O
-	// 			for(int k = 0; k<3;k++){
-	// 				int tx=j,ty=i;
-	// 				for(d=0;d<8;d++){
+
 	// 					tx+=dx[d];
-	// 					ty+=dy[d];
-	// 					if(field[ty][tx]==-2)
-	// 						ai_field[ty][tx]
-	// 				}
-	// 			}
-	// 		}
-    //     }
-	// }
